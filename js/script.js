04fb2c0b840470ba033215638b895773e3602f3e
Bùi Thành Long <btlong012@gmail.com>
2022-10-12 06:43:01 +0700

    de4

--- js/script.js
+++ js/script.js
@@ -9,4 +9,16 @@ $(document).ready(function () {
     $(".menu-content").removeClass("active");
     $(".cover").removeClass("active");
   });
+  //Product
+  $(".menu-products-inner a").click(function (e) {
+    e.preventDefault();
+    const tab_target = $(this).attr("tab-target");
+    // Xóa active của các product-list và product-tab a
+    $(".menu-products-inner a").removeClass("active");
+    $(".products").removeClass("active");
+    // /Khi click vào product-tab nào thì nó sẽ được thêm class active
+    // Đồng thời product list có id = giá trị của thuộc tính tab - target cũng được thêm class active
+    $(this).addClass("active");
+    $(tab_target).addClass("active");
+  });
 });
